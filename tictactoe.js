3673d3072b953ca3ef125a901831e71bce3153cb
Wesley Belleman <bellemanwesley@gmail.com>
2023-06-24 17:47:43 -0400

    just made moves random for now

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,12 @@
-alert("hello")
+var my_canvas = document.getElementById("myCanvas");
+my_canvas.onload = ttt_loader();
+var w = window.innerWidth;
+var h = window.innerHeight;
+var size = Math.min(w*0.66, h*0.66);
+var my_board = [[0,0,0],[0,0,0],[0,0,0]];
+var latest_move = "";
+
 function ttt_loader() {
-  var my_canvas = document.getElementById("myCanvas");
   var w = window.innerWidth;
   var h = window.innerHeight;
   var size = Math.min(w*0.66, h*0.66);
@@ -14,12 +20,62 @@ function ttt_loader() {
   draw_line(ctx,size/20,2*size/3,size-size/20,2*size/3);
   
   // Add event listener for `click` events.
-  my_canvas.addEventListener('click', function(event) {
+  my_canvas.addEventListener('click', ttt_click, false);
+
+  // Add event listener for `onmouseover` events. Will add this feature later.
+  //my_canvas.addEventListener('mouseover', ttt_hover, false);  
+}
+
+function ttt_click(event) {
+    var ctx = my_canvas.getContext("2d");
     var x = event.pageX - (w-size)/2,
         y = event.pageY - (h-size)/2;
     var move = [Math.floor(y/(size/3)),Math.floor(x/(size/3))];
-    draw_letter(ctx, "X", move[1], move[0]);
-}, false);
+    if (my_board[move[0]][move[1]]===0) {
+      //alert(check_board(my_board));
+      if (check_board(my_board)) {
+        draw_letter(ctx, "X", move[1], move[0]);
+        my_board[move[0]][move[1]] = 1;
+      }
+      //alert(check_board(my_board));
+      if (check_board(my_board)) {
+        var computer_move = get_move(my_board);
+        draw_letter(ctx, "O",computer_move[1],computer_move[0]);
+        my_board[computer_move[0]][computer_move[1]] = -1;
+      }
+      check_board(my_board);
+    }
+}
+
+function check_board(board, ctx) {
+  var winner = 0;
+  var ctx = my_canvas.getContext("2d");
+  for (var i=0; i<3; i++) {
+    if (board[i][0]===board[i][1]&&board[i][1]===board[i][2]&&board[i][2]!=0) {
+      winner = board[i][0];
+    }
+  }
+  for (i=0; i<3; i++) {
+    if (board[0][i]===board[1][i]&&board[1][i]===board[2][i]&&board[2][i]!=0) {
+      winner = board[0][i];
+    }
+  }
+  if (board[0][0]===board[1][1]&&board[1][1]===board[2][2]&&board[2][2]!=0) {
+    winner = board[0][0];
+  }
+  if (board[0][2]===board[1][1]&&board[1][1]===board[2][0]&&board[2][0]!=0) {
+    winner = board[0][2];
+  }
+  //alert(board);
+  //alert(winner);
+  if (winner === 0) {
+    return true;
+  } else {
+    my_canvas.removeEventListener('click', ttt_click);
+    //setTimeout(function(){ ttt_loader(); my_board = [[0,0,0],[0,0,0],[0,0,0]];}, 3000);
+    my_board = [[0,0,0],[0,0,0],[0,0,0]];
+    return false;
+  }
 }
 
 function draw_line(ctx, s_x,s_y,e_x,e_y) {
@@ -45,4 +101,18 @@ function draw_letter(ctx, letter, x, y) {
     2: size/1.15,
   };
   ctx.fillText(letter, x_dict[x], y_dict[y]);
+}
+
+function get_move(board) {
+  var moves = [];
+  for (var i=0; i<board.length; i++) {
+    for (var j=0; j<board[i].length; j++) {
+      if (board[i][j]===0) {
+        moves.push([i,j]);
+      }
+    }
+  }
+  var selected = Math.floor(Math.random()*moves.length);
+  var move = moves[selected];
+  return move;
 }
